41208614803233603195433888cdc881cf3546f6
yashV0201 <vardhan.yash0201@gmail.com>
2024-06-07 20:34:53 +0530

    added some flex!

--- 02_tailwindInteg/src/App.jsx
+++ 02_tailwindInteg/src/App.jsx
@@ -10,8 +10,14 @@ function App() {
   return (
     <>
       <h1 className='bg-slate-50 text-black p-4 rounded-lg mb-4'>Hi Man!</h1>
-      <Card userName="pikachu" image="https://cdn.pixabay.com/photo/2020/08/29/16/08/pikachu-5527377_1280.jpg" about="Electricity type pokemon" />
-      <Card userName="charmander" image="https://images.unsplash.com/photo-1567360425618-1594206637d2?q=80&w=2068&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" about="Fire type pokemon"/>
+      
+      <div className="flex justify-center gap-4">
+        <Card userName="pikachu" image="https://cdn.pixabay.com/photo/2020/08/29/16/08/pikachu-5527377_1280.jpg" about="Electricity type pokemon" />
+        <Card userName="charmander" image="https://images.unsplash.com/photo-1567360425618-1594206637d2?q=80&w=2068&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" about="Fire type pokemon"/>
+      </div>
+      
+      
+      
     </>
     
     
